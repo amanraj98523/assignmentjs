1f850aad0643b0af4e17c5c6e29847a56834869e
Aman Raj <166653226+amanraj98523@users.noreply.github.com>
2025-01-21 23:45:21 +0530

    Add files via upload

---
+++ assign1.js
@@ -0,0 +1,74 @@
+
+question:-1
+// // flattenArray([1, [2, [3, [4, 5]]]], 2);
+// // Expected output: [1, 2, 3, [4, 5]]
+
+// function flattenArray(arr,depth){
+//   return arr.reduce((acc,val)=>{
+//     if(Array.isArray(val) && depth>0){
+//       return acc.concat(flattenArray(val,depth-1));
+//     }
+//     return acc.concat(val);
+//   },[]);
+// }
+
+// console.log(flattenArray([1, [2, [3, [4, 5]]]], 2));
+
+
+
+
+
+// q1.b// let arr = [1, [2, [3, [4, 5]]]];
+// // Expected output: [1, 2, 3, 4, 5]
+
+// function flattenArray(arr){
+//   return arr.reduce((acc,val)=>
+//     Array.isArray(val) ? acc.concat(flattenArray(val)): acc.concat(val), []);
+// }
+// console.log(flattenArray(arr));
+
+
+question:- 2
+
+function flattenObject(obj, parentKey = "") {
+  const result = {};
+
+  for (const key in obj) {
+    const newKey = parentKey ? `${parentKey}.${key}` : key;
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      Object.assign(result, flattenObject(obj[key], newKey));
+    } else {
+      
+      result[newKey] = obj[key];
+    }
+  }
+
+  return result;
+}
+
+
+const input = {
+  user: {
+    info: {
+      name: "Alice",
+      age: 25
+    },
+    preferences: {
+      theme: "dark",
+      notifications: true
+    }
+  },
+  status: "active"
+};
+
+console.log(flattenObject(input));
+/*
+Expected Output:
+{
+  "user.info.name": "Alice",
+  "user.info.age": 25,
+  "user.preferences.theme": "dark",
+  "user.preferences.notifications": true,
+  "status": "active"
+}
+*/
